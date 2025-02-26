5b188715e45c81b3f138fa29dbffb00914d9248f
samuelpd22 <samuel050101@hotmail.com>
2025-02-26 18:52:14 -0300

    fix: redirect caso 'path' seja invalido

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from mypage.views import login_view, registro
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/login/', permanent=False)),  
     path('registro/', registro, name='registro'),
     path('login/', login_view, name='login'),
 ]
